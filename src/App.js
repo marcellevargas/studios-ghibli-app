ae84555abd3873c90e80536b5fb2015d919056db
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-24 10:26:50 -0300

    [part-1] - create responsive header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "./general/images/Studio_Ghibli_logo.png";
+import { FaBars } from "react-icons/fa";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header-container">
+        <img className="header-logo" src={logo} alt="Logo dos Studios Ghibli" />
+        <div className={`header-container__menu-content ${(isOpen ? 'expanded' : '')}`}>
+          <a
+            className="header-link"
+            href="https://meyerweb.com/eric/tools/css/reset/"
+          >
+            Filmes
+          </a>
+          <a
+            className="header-link"
+            href="https://meyerweb.com/eric/tools/css/reset/"
+          >
+            Atores
+          </a>
+          <a
+            className="header-link"
+            href="https://meyerweb.com/eric/tools/css/reset/"
+          >
+            Locais
+          </a>
+        </div>
+
+        <button className="hamburguer-icon" onClick={() => setOpen(!isOpen)}>
+          <FaBars />
+        </button>
       </header>
     </div>
   );
